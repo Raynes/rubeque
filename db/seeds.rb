160d025269c19765b6a927cba4ec2e1f5394fa32
David Davis <davidd@scimedsolutions.com>
2012-01-13 15:55:05 -0500

    Worked on flash messages and added a problem

--- db/seeds.rb
+++ db/seeds.rb
@@ -80,7 +80,7 @@ Problem.create!(_id: 7,
  assert_equal 55, fibo_finder(10)
  code_block
                  )
-                 
+
 Problem.create!(_id: 8,
                difficulty: 'medium',
                title: "Pigs fly;  wait, no they don't.",
@@ -95,6 +95,24 @@ Problem.create!(_id: 8,
                  assert_equal do_pigs_fly?, false
                eos
                )
+
+Problem.create!(_id: 9,
+                  difficulty: "medium",
+                  title: "Happy numbers",
+                  instructions: "Happy numbers are positive integers that follow a particular formula: take each individual digit,
+                                 square it, and then sum the squares to get a new number. Repeat with the new number and eventually,
+                                 you might get to a number whose squared sum is 1. This is a happy number. An unhappy number (or
+                                 sad number) is one that loops endlessly. Write a function that determines if a number is happy or not.",
+                  approved: true,
+                  code: <<-code_block
+ __
+
+ assert_equal happy_number?(7), true
+ assert_equal happy_number?(986543210), true
+ assert_equal happy_number?(2), false
+ assert_equal happy_number?(3), false
+ code_block
+                 )
 
 User.delete_all
 User.create!(:username => 'admin',
