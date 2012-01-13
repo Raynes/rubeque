d665e87267c216795a4482f239d849635d0e0187
David Davis <davidd@scimedsolutions.com>
2012-01-13 16:19:27 -0500

    Worked on the problem id problem

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,25 +1,27 @@
 class Problem
   include Mongoid::Document
   include Mongoid::Timestamps
-  scope :approved, proc{ where(approved: true )}
-  scope :unapproved, proc{ any_of({approved: nil}, {approved: false}) }
+  field :_id, type: Integer
+  key :_id
+
   field :title
   field :instructions
   field :code
   field :difficulty
   field :approved, type: Boolean
-  field :_id, type: Integer
-  key :_id
-
-  before_create :check_problem_id
 
   references_many :solutions
+  referenced_in :creator, class_name: "User"
+
+  scope :approved, proc{ where(approved: true ) }
+  scope :unapproved, proc{ any_of({approved: nil}, {approved: false}) }
+
+  before_create :automatically_approve
 
   DIFFICULTY_LEVELS = ["easy", "medium", "hard"]
 
   validates_presence_of :title, :code, :difficulty
-  
-  
+
   def to_s
     "#{title}"
   end
@@ -30,11 +32,7 @@ class Problem
 
   private
 
-    def check_problem_id
-      existing_problem = Problem.find(self._id) rescue nil
-      if existing_problem
-        errors.add(:base, "There was a problem id collision. Please try again.")
-        return false
-      end
+    def automatically_approve
+      approved = true if creator && creator.admin?
     end
 end
