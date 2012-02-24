d892c0d49b0ee2f279a8376eb30daab49360b8fe
David Davis <davidd@scimedsolutions.com>
2012-02-24 18:02:37 -0500

    Added Google Analytics for rubeque.com #66

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,19 @@ module ApplicationHelper
     ((params[:page] || 1).to_i - 1) * per_page
   end
 
+  def google_analytics_js
+    "<script type='text/javascript'>
+
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', '#{ENV['google_analytics_id']}']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+
+    </script>"
+  end
 end
