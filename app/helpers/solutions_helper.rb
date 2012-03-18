527ce57a202bc1a5c8a18b58d88c7a762ec8647b
David Davis <davidd@scimedsolutions.com>
2012-03-17 20:05:20 -0400

    Fixed twitter link

--- app/helpers/solutions_helper.rb
+++ app/helpers/solutions_helper.rb
@@ -1,10 +1,11 @@
 module SolutionsHelper
 
   def twitter_link(problem, paste)
-    url = "http://rubeque.com/problems/#{problem.id}"
-    text = URI.escape("Check out my solution to '#{problem.title}' (#{url}) on #rubeque: #{paste}")
+    url = URI.escape("http://rubeque.com/problems/#{problem.id}")
+    text = URI.escape("Check out my solution to '#{problem.title}' (#{url}) on #rubeque")
     link = "http://twitter.com/share?"
     link += "text=#{text}"
+    link += "url=#{paste}"
     link_to "Edit your tweet on Twitter", link, class: "twitter"
   end
 end
